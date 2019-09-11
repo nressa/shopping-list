55f41c53655c520d9cad286207e4b094e7b92f6e
Ressa Magbanua <mressanova@gmail.com>
2019-09-11 23:08:06 +0800

    v-for and guidelines for array in props

--- coffee plan/app.js
+++ coffee plan/app.js
@@ -1,8 +1,19 @@
 Vue.component('card-plan', {
 	template: '#card-plan-template',
-	props: ['name']
+	props: {
+				name: String, 
+				description: String
+	}
 })
 
 new Vue({
-	el: '#plan'
+	el: '#plan',
+	data: {
+		plans: 	[
+					{label: "Brown Coffee", description: "Quis ad proident ut ut aute dolor aliqua in voluptate elit sint culpa culpa qui eu ad excepteur quis."},
+					{label: "Espresso", description: "Deserunt duis sint tempor in laborum dolore velit est."},	
+					{label: "Mocha", description: "Id ullamco sunt sint ut voluptate magna quis in officia esse qui in occaecat do aute aliqua culpa."},
+					{label: "Vanilla", description: "Lorem ipsum dolore quis dolor magna tempor deserunt nulla culpa sit reprehenderit esse tempor sed officia."}
+				]
+	}
 })
