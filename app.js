8dd79e4eef61f5dda38110c42727eacd4e327370
Ressa Magbanua <mressanova@gmail.com>
2019-09-11 14:08:55 +0800

    nested components

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+Vue.component('item-section', {
+	template: '#item-section-template',
+	data(){
+		return {
+			items:[
+				{
+					name: {
+						type: String,
+						required: true,
+						default: 'party hats',
+					},
+					description: '24 Pcs Mickey Mouse or Disney Pricess Design',
+				},
+				{
+					name: {
+						type: String,
+						required: true,
+						default: 'monoblock chairs',
+					},
+					description: '25 pcs monoblock chair for rent',
+				},
+				{
+					name: {
+						type: String,
+						required: true,
+						default: 'paper plates',
+					},
+					description: '20 pieces of paper plates in 1 pack',
+				}
+			]
+		}
+	}
+
+})
+
+
 Vue.component('Itemcard', {
 	template: '#item-card-template',
 	props: ['name' ,'description']
@@ -12,31 +48,5 @@ var shoppingList = new Vue({
 		searchItem: null,
 		newItem: '',
 		newItemDescription: '',
-		items:[
-			{
-				name: {
-					type: String,
-					required: true,
-					default: 'party hats',
-				},
-				description: '24 Pcs Mickey Mouse or Disney Pricess Design',
-			},
-			{
-				name: {
-					type: String,
-					required: true,
-					default: 'monoblock chairs',
-				},
-				description: '25 pcs monoblock chair for rent',
-			},
-			{
-				name: {
-					type: String,
-					required: true,
-					default: 'paper plates',
-				},
-				description: '20 pieces of paper plates in 1 pack',
-			}
-		]
 	},
 });
