ed8e4f1f82486e7a738d1859f2bf9faa7c8ba942
Ressa Magbanua <mressanova@gmail.com>
2019-09-11 12:32:16 +0800

    create x-template

--- app.js
+++ app.js
@@ -1,60 +1,7 @@
-var shoppingList = new Vue({
-	el: '#shopping-list',
-	data: {
-		header: 'shopping list',
-		state: 'default',
-		searchItem: null,
-		newItem: '',
-		newItemDescription: '',
-		items:[
-			{
-				label: 'party hats',
-				description: '24 Pcs Mickey Mouse or Disney Pricess Design',
-				purchased: false,
-				priority: false,
-			},
-			{
-				label: 'monoblock chairs',
-				description: '25 pcs monoblock chair for rent',
-				purchased: true,
-				priority: false,
-			},
-			{
-				label: 'paper plates',
-				description: '20 pieces of paper plates in 1 pack',
-				purchased: false,
-				priority: true,
-			}
-		]
-	},
-	computed: { 
-		// computed properties change data for presentation wise
-		//  Must not alter or change existing data
-		reversedItems() {
-			return this.items.slice(0).reverse();
-		}
+Vue.component('Itemcard', {
+	template: '#item-card-template'
+})
 
-	},
-	methods: {
-		saveItem: function(){
-			this.items.push({
-				label: this.newItem,
-				description: this.newItemDescription,
-				purchased: false,
-				priority: false
-			});
-			this.newItem = "";
-		},
-		changeState: function(newState){
-			this.state = newState;
-			this.newItem = "";
-			this.newItemDescription = "";
-		},
-        togglePurchased: function(item) {
-            item.purchased = !item.purchased
-        },
-        togglePriority: function(item) {
-            item.priority = !item.priority
-        },
-	}
-});
+new Vue({
+	el: '#shopping-list'
+})
