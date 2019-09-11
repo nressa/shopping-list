5d54fcc85f228ebbe479277f1aeb1b9851a1eab7
Ressa Magbanua <mressanova@gmail.com>
2019-09-11 13:03:00 +0800

    transfer template to componets and add props

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 Vue.component('Itemcard', {
-	template: '#item-card-template'
+	template: '#item-card-template',
+	props: ['name' ,'description']
+
 })
 
-new Vue({
-	el: '#shopping-list'
-})
+var shoppingList = new Vue({
+	el: '#shopping-list',
+	data: {
+		header: 'Shopping List',
+		state: 'default',
+		searchItem: null,
+		newItem: '',
+		newItemDescription: '',
+		items:[
+			{
+				name: 'party hats',
+				description: '24 Pcs Mickey Mouse or Disney Pricess Design',
+			},
+			{
+				name: 'monoblock chairs',
+				description: '25 pcs monoblock chair for rent',
+			},
+			{
+				name: 'paper plates',
+				description: '20 pieces of paper plates in 1 pack',
+			}
+		]
+	},
+});
