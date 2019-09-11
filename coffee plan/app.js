22078d0a47c7670250c2a8ccbe3ae1f94ad88e8d
Ressa Magbanua <mressanova@gmail.com>
2019-09-11 23:45:42 +0800

    register global component to local

--- coffee plan/app.js
+++ coffee plan/app.js
@@ -1,18 +1,5 @@
-Vue.component('plan-section', {
-	template: '#plan-section-template',
-	data() {
-		return {
-				plans: 	[
-							{label: "Brown Coffee", description: "Quis ad proident ut ut aute dolor aliqua in voluptate elit sint culpa culpa qui eu ad excepteur quis."},
-							{label: "Espresso", description: "Deserunt duis sint tempor in laborum dolore velit est."},	
-							{label: "Mocha", description: "Id ullamco sunt sint ut voluptate magna quis in officia esse qui in occaecat do aute aliqua culpa."},
-							{label: "Vanilla", description: "Lorem ipsum dolore quis dolor magna tempor deserunt nulla culpa sit reprehenderit esse tempor sed officia."}
-						]
-		}
-	}
-})
 
-Vue.component('card-plan', {
+let CardPlanComponent = {
 	template: '#card-plan-template',
 	props: {
 				name: {
@@ -24,8 +11,30 @@ Vue.component('card-plan', {
 					required: true
 				}, 
 	}
-})
+}
+
+let PlanSectionComponent = {
+	template: '#plan-section-template',
+	components: {
+		'card-plan': CardPlanComponent,
+	},
+	data() {
+		return {
+				plans: 	[
+							{label: "Brown Coffee", description: "Quis ad proident ut ut aute dolor aliqua in voluptate elit sint culpa culpa qui eu ad excepteur quis."},
+							{label: "Espresso", description: "Deserunt duis sint tempor in laborum dolore velit est."},	
+							{label: "Mocha", description: "Id ullamco sunt sint ut voluptate magna quis in officia esse qui in occaecat do aute aliqua culpa."},
+							{label: "Vanilla", description: "Lorem ipsum dolore quis dolor magna tempor deserunt nulla culpa sit reprehenderit esse tempor sed officia."}
+						]
+		}
+	}
+}
+
 
 new Vue({
-	el: '#plan'
+	el: '#plan',
+	components: {
+		'plan-section': PlanSectionComponent,
+	}
 })
+
