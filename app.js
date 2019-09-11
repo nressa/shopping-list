df690815bf37b824fb67501e20da52c5a59e04e2
Ressa Magbanua <mressanova@gmail.com>
2019-09-11 16:09:49 +0800

    error in object in script

--- app.js
+++ app.js
@@ -1,52 +1,28 @@
-Vue.component('item-section', {
-	template: '#item-section-template',
-	data(){
-		return {
-			items:[
-				{
-					name: {
-						type: String,
-						required: true,
-						default: 'party hats',
-					},
-					description: '24 Pcs Mickey Mouse or Disney Pricess Design',
-				},
-				{
-					name: {
-						type: String,
-						required: true,
-						default: 'monoblock chairs',
-					},
-					description: '25 pcs monoblock chair for rent',
-				},
-				{
-					name: {
-						type: String,
-						required: true,
-						default: 'paper plates',
-					},
-					description: '20 pieces of paper plates in 1 pack',
-				}
-			]
-		}
-	}
-
+Vue.component('Plan', {
+	template: '#plan-card-template',
+	props: ['name', 'description']
 })
 
-
-Vue.component('Itemcard', {
-	template: '#item-card-template',
-	props: ['name' ,'description']
-
-})
-
-var shoppingList = new Vue({
+new Vue({
 	el: '#shopping-list',
 	data: {
-		header: 'Shopping List',
-		state: 'default',
-		searchItem: null,
-		newItem: '',
-		newItemDescription: '',
-	},
-});
+		plans: [ 
+			{
+				name: 'Caffè Americano',
+				description: 'You can make this type of coffee quite simply by adding hot water to a shot of espresso coffee. It has been said that American soldiers during the Second World War would make this type of coffee to make their beverages last longer. It was then (apparently) adopted by American baristas after the war.',
+			},
+			{
+				name: 'Café Latte (or Café au lait)',
+				description: 'You can make this type of coffee quite simply by adding hot water to a shot of espresso coffee. It has been said that American soldiers during the Second World War would make this type of coffee to make their beverages last longer. It was then (apparently) adopted by American baristas after the war.',
+			},
+			{
+				name: 'Cappuccino',
+				description: 'Possibly the most popular type of coffee in the world, a cappuccino consists of three layers (kind of like a cake). The first is a shot of espresso, then a shot of steamed milk, and finally the barista adds a layer of frothed, foamy milk. This final layer can also be topped with chocolate shavings or powder. Traditionally, Italians would consume this type of coffee at breakfast.',
+			},
+			{
+				name: 'Espresso',
+				description: 'To make an espresso, shoot boiling water under high pressure through finely ground up coffee beans and then pour into a tiny mug. Sounds simple right? Well, it’s surprisingly difficult to master. Espressos are the purest coffee experience you can get, and while they’re not for everyone, it can be a truly singular drinking experience when you find a good brew.',
+			},
+		]
+	}
+})
